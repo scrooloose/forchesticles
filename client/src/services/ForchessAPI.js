c2bc388d9fac2d837b738e498e9bea35e4460052
Martin Grenfell <martin.grenfell@gmail.com>
2017-12-11 17:06:08 +0000

    use env file for api endpoint

--- client/src/services/ForchessAPI.js
+++ client/src/services/ForchessAPI.js
@@ -1,38 +1,42 @@
 class ForchessAPI {
+  constructor({baseurl}) {
+    this.baseurl = baseurl;
+  }
+
   getGameIds() {
-    return this._getPromiseForGet('http://localhost:5000/api/v1/games/');
+    return this._getPromiseForGet('/games');
   }
 
   getGame(id) {
-    return this._getPromiseForGet('http://localhost:5000/api/v1/games/' + id);
+    return this._getPromiseForGet('/games/' + id);
   }
 
   sendMove({gameId, from, to}) {
     return this._getPromiseForPost({
-      url: 'http://localhost:5000/api/v1/games/' + gameId + '/moves',
+      url: '/games/' + gameId + '/moves',
       body: {from: from, to: to}
     });
   }
 
   newGame() {
-    return this._getPromiseForPost({ url: 'http://localhost:5000/api/v1/games/' });
+    return this._getPromiseForPost({ url: '/games/' });
   }
 
   undoMove(gameId) {
-    return this._getPromiseForPost({ url: 'http://localhost:5000/api/v1/games/' + gameId + '/undos' });
+    return this._getPromiseForPost({ url: '/games/' + gameId + '/undos' });
   }
 
   deleteGame(gameId) {
-    return this._getPromiseForPost({ url: 'http://localhost:5000/api/v1/games/' + gameId + '/delete' });
+    return this._getPromiseForPost({ url: '/games/' + gameId + '/delete' });
   }
 
   _getPromiseForGet(url) {
-    return fetch(url).then((resp) => resp.json());
+    return fetch(this.baseurl + url).then((resp) => resp.json());
   }
 
   _getPromiseForPost({url, body={}}) {
     return fetch(
-        url, {
+        this.baseurl + url, {
         headers: { 'Content-type': 'application/json' },
         method: 'POST',
         body: JSON.stringify(body)
@@ -41,4 +45,4 @@ class ForchessAPI {
   }
 }
 
-module.exports = new ForchessAPI();
+module.exports = new ForchessAPI({baseurl: process.env.REACT_APP_FORCHESS_API_BASEURL});
